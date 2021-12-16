2d0121a40e4b06e42e1238efc87b1237b2a44c39
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2021-12-16 23:23:20 +0300

    Make icons rotate on click on question previews, fix bug with text picking out of the question

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log("You are at " + window.location);
+var preview = document.getElementsByClassName("faq-card__preview-container");
+var popupIcon = document.getElementsByClassName("faq-card__pop-up-icon");
+
+var _loop = function _loop(i) {
+  preview[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var popup = this.nextElementSibling;
+
+    if (popup.style.display === "block") {
+      popup.style.display = "none";
+      popupIcon[i].style.transform = "rotate(360deg)";
+    } else {
+      popup.style.display = "block";
+      preview[i].style.filter = "none";
+      popupIcon[i].style.transform = "rotate(180deg)";
+    }
+  });
+};
+
+for (var i = 0; i < preview.length; i++) {
+  _loop(i);
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("You are at " +window.location);
+const preview = document.getElementsByClassName("faq-card__preview-container");
+const popupIcon = document.getElementsByClassName("faq-card__pop-up-icon");
+
+for (let i = 0; i < preview.length; i++) {
+    preview[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        
+        const popup = this.nextElementSibling;
+        if (popup.style.display === "block") {
+            popup.style.display = "none";
+            popupIcon[i].style.transform = "rotate(360deg)";
+        } else {
+            popup.style.display = "block";
+            preview[i].style.filter = "none";
+            popupIcon[i].style.transform = "rotate(180deg)";
+        }
+    });
+}
