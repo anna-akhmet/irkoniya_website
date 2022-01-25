a847e88db16bc647f85988f0efc058cf56ad0d91
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-01-25 11:26:58 +0300

    Add a variable to the document and a code to fix bug with two colums instead of three in horse and horse riding sections
    Also added an expression css to paddings widths of horses and horse riding sections calculating paddings excluding scrollbar width

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -33,3 +33,14 @@ contactButtons.forEach(function (button) {
     });
   });
 });
+
+function _calculateScrollbarWidth() {
+  document.documentElement.style.setProperty('--scrollbar-width', window.innerWidth - document.documentElement.clientWidth + "px");
+} // recalculate on resize
+
+
+window.addEventListener('resize', _calculateScrollbarWidth, false); // recalculate on dom load
+
+document.addEventListener('DOMContentLoaded', _calculateScrollbarWidth, false); // recalculate on load (assets loaded as well)
+
+window.addEventListener('load', _calculateScrollbarWidth);
--- js/script.js
+++ js/script.js
@@ -27,3 +27,12 @@ contactButtons.forEach(button => button.addEventListener("click",function() {
     });
 }));
 
+function _calculateScrollbarWidth() {
+    document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+  }
+  // recalculate on resize
+  window.addEventListener('resize', _calculateScrollbarWidth, false);
+  // recalculate on dom load
+  document.addEventListener('DOMContentLoaded', _calculateScrollbarWidth, false); 
+  // recalculate on load (assets loaded as well)
+  window.addEventListener('load', _calculateScrollbarWidth);
